5bf15799bd654ab48299cd67d8b7861cf6718f16
León Ceballos <leon.ceballos@wolox.co>
2018-11-07 17:13:39 -0500

    delay works well

--- react/finger-exercises/ecmascript/02 - promises/index.js
+++ react/finger-exercises/ecmascript/02 - promises/index.js
@@ -1,7 +1,12 @@
 // Hint: use setInterval, create a new Promise and measure time with Date.now()
 
-export function delay() {
-
+export function delay(time) {
+  const start = Date.now();
+  return new Promise((resolve) => {
+    setInterval(() => {
+      resolve(Date.now() - start);
+    }, time);
+  });
 }
 
 export function asyncDelay() {
