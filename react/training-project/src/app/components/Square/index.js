7df72dd5fef1cfd6c81744a06e41a4ab3bc28323
León Ceballos <leon.ceballos@wolox.co>
2018-11-13 17:06:59 -0500

    Lifting State Up

--- react/training-project/src/app/components/Square/index.js
+++ react/training-project/src/app/components/Square/index.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import style from './styles.scss';
 
 class Square extends Component {
-  state = {
-    value: null
-  };
   render() {
     return (
-      <button className={style.square} onClick={() => this.setState({ value: 'X' })}>
-        {this.state.value}
+      <button className={style.square} onClick={this.props.onClick}>
+        {this.props.value}
       </button>
     );
   }
 }
+
+Square.propTypes = {
+  value: PropTypes.string,
+  onClick: PropTypes.func
+};
 
 export default Square;
