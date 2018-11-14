02cf9a879c637fad3dfe71c14cbd248ecd4ee64f
León Ceballos <leon.ceballos@wolox.co>
2018-11-14 09:51:08 -0500

    Declaring a Winner

--- react/training-project/src/app/components/Board/index.js
+++ react/training-project/src/app/components/Board/index.js
@@ -11,15 +11,27 @@ class Board extends Component {
   };
   handleCick = i => {
     const statesquares = this.state.squares.slice();
+    if (this.calculateWinner(statesquares) || statesquares[i]) return;
     statesquares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: statesquares,
       xIsNext: !this.state.xIsNext
     });
   };
+  calculateWinner = squares => {
+    const lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    for (let i = 0; i < lines.length; i += 1) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
   renderSquare = i => <Square value={this.state.squares[i]} key={i} onClick={() => this.handleCick(i)} />;
   render() {
-    const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+    const winner = this.calculateWinner(this.state.squares);
+    const status = winner ? `Winner: ${winner}` : `Next Player ${this.state.xIsNext ? 'X' : 'O'}`;
     const NUMBER_OF_COLUMNS = 3;
     return (
       <React.Fragment>
