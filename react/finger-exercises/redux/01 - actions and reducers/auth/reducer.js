b39a721b77cfc1d357830e3e4afffbd2eb75461a
León Ceballos <leon.ceballos@wolox.co>
2018-11-19 16:37:34 -0500

    logs in

--- react/finger-exercises/redux/01 - actions and reducers/auth/reducer.js
+++ react/finger-exercises/redux/01 - actions and reducers/auth/reducer.js
@@ -1,10 +1,21 @@
+import { actions } from './actions';
+
 const initialState = {
   email: null,
   token: null
 };
 
-function reducer(state = initialState) {
-  return state;
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case actions.LOGIN:
+      return {
+        ...state,
+        email: action.payload.email,
+        token: action.payload.token
+      };
+    default:
+      return state;
+  }
 }
 
 export default reducer;
