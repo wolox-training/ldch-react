7f2980d792c65a04bf897b95c3c8e50941494a4f
León Ceballos <leon.ceballos@wolox.co>
2018-11-30 10:44:22 -0500

    styling a bit the topbar

--- react/training-project/src/app/screens/AuthorizedRoute/components/Topbar/index.js
+++ react/training-project/src/app/screens/AuthorizedRoute/components/Topbar/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import style from './styles.scss';
+
 function Topbar() {
   return (
-    <nav>
-      <NavLink to="/app/game">Play</NavLink>
-      <NavLink to="/app/Profile">Profile</NavLink>
+    <nav className={style.navbar}>
+      <NavLink className={style.navbarItem} activeClassName={style.active} to="/app/game">
+        Play
+      </NavLink>
+      <NavLink className={style.navbarItem} activeClassName={style.active} to="/app/Profile">
+        Profile
+      </NavLink>
+      <button type="button" className={style.navbarButton}>
+        Log out
+      </button>
     </nav>
   );
 }
