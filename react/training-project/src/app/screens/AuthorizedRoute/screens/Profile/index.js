a21530967fb1569593dde214c3c77900ab4da72d
León Ceballos <leon.ceballos@wolox.co>
2018-12-03 11:10:04 -0500

    simple use of react-group-transitions

--- react/training-project/src/app/screens/AuthorizedRoute/screens/Profile/index.js
+++ react/training-project/src/app/screens/AuthorizedRoute/screens/Profile/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { CSSTransition } from 'react-transition-group';
 
-function Profile() {
-  return <h1>This is the Profile Screen!</h1>;
+import style from './styles.scss';
+
+function Profile({ show }) {
+  return (
+    <CSSTransition
+      in={show}
+      appear
+      timeout={600}
+      classNames={{
+        appear: style.fadeAppear,
+        appearActive: style.fadeAppearActive
+      }}
+    >
+      <h1>This is the Profile Screen!</h1>
+    </CSSTransition>
+  );
 }
 
 export default Profile;
