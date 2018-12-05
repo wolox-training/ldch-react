40c23ff235dd1c20f1a454f735f1c280772569bb
León Ceballos <leon.ceballos@wolox.co>
2018-12-05 10:15:04 -0500

    solving comments PR17

---
+++ react/training-project/src/app/screens/AuthorizedRoute/components/Topbar/constants.js
@@ -0,0 +1,3 @@
+export const PLAY = 'Play';
+export const PROFILE = 'Proofile';
+export const LOGOUT = 'Log out';
--- react/training-project/src/app/screens/AuthorizedRoute/components/Topbar/index.js
+++ react/training-project/src/app/screens/AuthorizedRoute/components/Topbar/index.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import { GAME, PROFILE as PROFILE_PATH } from '~components/Routes/constants';
+
 import style from './styles.scss';
+import { PLAY, PROFILE, LOGOUT } from './constants';
 
 function Topbar({ logOut }) {
   return (
     <nav className={style.navbar}>
-      <NavLink className={style.navbarItem} activeClassName={style.active} to="/app/game">
-        Play
+      <NavLink className={style.navbarItem} activeClassName={style.active} to={GAME}>
+        {PLAY}
       </NavLink>
-      <NavLink className={style.navbarItem} activeClassName={style.active} to="/app/Profile">
-        Profile
+      <NavLink className={style.navbarItem} activeClassName={style.active} to={PROFILE_PATH}>
+        {PROFILE}
       </NavLink>
       <button type="button" onClick={logOut} className={style.navbarButton}>
-        Log out
+        {LOGOUT}
       </button>
     </nav>
   );
